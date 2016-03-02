e9cd5c299ae67d71e781218107504997b888a386
Xi <contact@xi-liu.com>
2016-03-03 01:05:59 +1000

    Fix dependencies

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,7 +3,7 @@ $(document).foundation();
 // Declare app level module which depends on filters, and services
 var app = angular.module('HCBPrograms', ['ngResource', 'ngRoute', 'ui-notification']);
 
-angular.module('HCBPrograms').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider, NotificationProvider) {
+angular.module('HCBPrograms').config(['$routeProvider', '$locationProvider', 'NotificationProvider', function($routeProvider, $locationProvider, NotificationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
         .when("/", { templateUrl: "partials/index.jade", controller: "HomeCtrl" })
